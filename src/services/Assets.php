740f81c5dbfe46da5e973ac5acf71f8750fd8ffc
Eric Rosas <ericdrosas@gmail.com>
2021-11-16 15:05:26 -0700

    Disabled api key that was left in code and replaced it with env var references EPO-5590

--- src/services/Assets.php
+++ src/services/Assets.php
@@ -9,6 +9,12 @@ use craft\helpers\Json;
 
 class Assets extends Component
 {
+
+    private static $appId = getenv("CANTO_APP_ID");
+
+    private static $secretKey = getenv("CANTO_SECRET_KEY");
+
+
     public function init() {
         parent::init();
     }
@@ -47,7 +53,7 @@ class Assets extends Component
         'base_uri' => 'https://oauth.canto.com',
         ]);
 
-        $response = $client->post('/oauth/api/oauth2/token?app_id=d1dc81ae5d9f45e2a57dd2b6c5d19e19&app_secret=437696e1503146df83130ba0d646c3626167b560ea9b4fdf860d832620260dff&grant_type=client_credentials');
+        $response = $client->post('/oauth/api/oauth2/token?app_id=' . $appId . '&app_secret=' . $secretKey , '&grant_type=client_credentials');
         $body = $response->getBody();
 
         // Depending on the API...
