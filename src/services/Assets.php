b24a428c63e7e376fd2b40819cec95e362834cf8
Eric Rosas <ericdrosas@gmail.com>
2021-11-16 14:45:26 -0700

    Added a test harness button to grab auth token EPO-5590

--- src/services/Assets.php
+++ src/services/Assets.php
@@ -5,6 +5,7 @@ use Craft;
 use yii\base\Component;
 use craft\elements\Asset;
 use rosas\dam\services\Elements;
+use craft\helpers\Json;
 
 class Assets extends Component
 {
@@ -30,11 +31,32 @@ class Assets extends Component
 
         $elements = new Elements();
         // Don't validate required custom fields
-        $success = $elements->saveElement($newAsset, false);
-        echo "\n\n Rosas - success : " . $success . "\n\n";
+        //$success = $elements->saveElement($newAsset, false);
+        //echo "\n\n Rosas - success : " . $success . "\n\n";
 
 
-    public function testAssetQuery() {
-        return Craft::$app->getElements()->getElementById(1409);
     }
+
+    /**
+     * Test harness function for authenticating (grabbing an auth token)
+     * from the Canto API
+     */ 
+    public function testAuth() {
+        // https://docs.guzzlephp.org/en/stable/quickstart.html
+        $client = Craft::createGuzzleClient([
+        'base_uri' => 'https://oauth.canto.com',
+        ]);
+
+        $response = $client->post('/oauth/api/oauth2/token?app_id=d1dc81ae5d9f45e2a57dd2b6c5d19e19&app_secret=437696e1503146df83130ba0d646c3626167b560ea9b4fdf860d832620260dff&grant_type=client_credentials');
+        $body = $response->getBody();
+
+        // Depending on the API...
+        $data = Json::decodeIfJson($body);
+        return $body;
+    }
+
+
+    // public function testAssetQuery() {
+    //     return Craft::$app->getElements()->getElementById(1409);
+    // }
 }
