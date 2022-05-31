454f7972337ef3059b253c5d40361aa7bd08b9ee
Eric Rosas <ericdrosas@gmail.com>
2022-05-31 14:13:24 -0700

    Now returning metadata as part of standard query EPO-6391

--- src/gql/types/DAMAssetType.php
+++ src/gql/types/DAMAssetType.php
@@ -4,18 +4,10 @@ namespace rosas\dam\gql\types;
 
 use Craft;
 use rosas\dam\gql\interfaces\DAMAssetInterface;
-
 use craft\gql\base\ObjectType;
 
 use GraphQL\Type\Definition\ResolveInfo;
 
-/**
- * Class SeomaticType
- *
- * @author    nystudio107
- * @package   Seomatic
- * @since     3.2.8
- */
 class DAMAssetType extends ObjectType {
     /**
      * @inheritdoc
@@ -32,6 +24,36 @@ class DAMAssetType extends ObjectType {
      * @inheritdoc
      */
     protected function resolve($source, $arguments, $context, ResolveInfo $resolveInfo) {
-        return $source[$resolveInfo->fieldName];
+        
+        if(array_key_exists($resolveInfo->fieldName, $source)) {
+            return $source[$resolveInfo->fieldName];
+        } else if($resolveInfo->fieldName == "damMetadata") {
+            $metadata = $this->getAssetMetadataByAssetId($source->id);
+            return $metadata;
+        } else {
+            try {
+                $resolvedValue = $source[$resolveInfo->fieldName];
+                return $resolvedValue;
+            } catch (Exception $e) {
+                return null;
+            }
+        }
+        
+    }
+
+    public static function getAssetMetadataByAssetId($assetId) {
+        $rows = AssetMetadata::find()
+        ->where(['"assetId"' => $assetId])
+        ->all();
+
+        $res = [];
+        $currentId = 0;
+        foreach($rows as $row) {
+            $metadataRow = [];
+            $metadataRow["metadataKey"] = $row["dam_meta_key"];
+            $metadataRow["metadataValue"] = $row["dam_meta_value"];
+            array_push($res, $metadataRow);
+        }
+        return $res;
     }
 }
--- src/gql/types/generators/DAMAssetGenerator.php
+++ src/gql/types/generators/DAMAssetGenerator.php
@@ -79,4 +79,5 @@ class DAMAssetGenerator implements GeneratorInterface
         ]));
 
     }
+
 }
